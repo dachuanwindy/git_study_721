757b36f802190fbf85a7e8aa562de5899e18b7cf
sunfengchuan <sunfengchuan@58.com>
2020-01-04 01:40:12 +0800

    new project

---
+++ gitwebtest/src/main/java/com/bj/tomato/gitwebtest/GitwebtestApplication.java
@@ -0,0 +1,13 @@
+package com.bj.tomato.gitwebtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitwebtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitwebtestApplication.class, args);
+    }
+
+}
---
+++ gitwebtest/src/test/java/com/bj/tomato/gitwebtest/GitwebtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bj.tomato.gitwebtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitwebtestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
