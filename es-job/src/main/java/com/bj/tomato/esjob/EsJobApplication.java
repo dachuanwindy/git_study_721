33404e3c055cb334a0607d00f4c67244af46e67b
sunfengchuan <sunfengchuan@58.com>
2020-02-22 23:04:45 +0800

    es-job工程搭建，实现分布式任务调度

---
+++ es-job/src/main/java/com/bj/tomato/esjob/EsJobApplication.java
@@ -0,0 +1,13 @@
+package com.bj.tomato.esjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsJobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsJobApplication.class, args);
+    }
+
+}
